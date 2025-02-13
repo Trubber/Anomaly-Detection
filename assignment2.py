6c2745f8f01be4b3e9ed309c5be2030cb5b5208f
Trubber <73858351+Trubber@users.noreply.github.com>
2025-02-13 12:53:46 -0500

    Started working on pca

--- assignment2.py
+++ assignment2.py
@@ -1,6 +1,21 @@
 import numpy as np
-import sklearn as skl
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
 
+training = np.load("./KDD99/training_normal.npy")
+
+scale = StandardScaler()
+scaled_data = scale.fit_transform(training)
 
 
+pca = PCA(n_components=2)
+pca.fit(scaled_data)
+print(pca.get_feature_names_out())
+
+
+#plt.xlabel("Test x")
+#plt.ylabel("Test y")
+
+#plt.show()
